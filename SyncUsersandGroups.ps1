3cfc1e932ac086d9ee32459f9ea0abcd1c44cff9
maxxys-mzierer <88876471+maxxys-mzierer@users.noreply.github.com>
2023-12-13 11:32:42 +0100

    query ad1 users
    Abfrage der Benutzer aus AD1 hinzugefügt

--- SyncUsersandGroups.ps1
+++ SyncUsersandGroups.ps1
@@ -75,6 +75,7 @@ function  Write-Log {
 function Get-Group {
   param (
     $Credentials = "none",
+    $SearchBase = "none",
     $Server = "none",
     $Pattern = "none"
   )
@@ -82,7 +83,7 @@ function Get-Group {
   Write-Log -Message ('authenticate at {0} using {1}' -f $Server,$Credentials.Username)
   $Filter = "Name -like '" + $Pattern + "'"
   [array]$ADGroups = @()
-  $ADGroups = Get-ADGroup -Server $Server -Credential $Credentials -Filter $Filter
+  $ADGroups = Get-ADGroup -Server $Server -Credential $Credentials -Filter $Filter -SearchBase $SearchBase
 
   Write-Log -Message "cleanup"
   Remove-Variable -Name Credentials
@@ -114,14 +115,18 @@ Write-Log -Message "start region read data from XML file"
 
 # prepare Variables
 [string]$CurrentUser = [Security.Principal.WindowsIdentity]::GetCurrent().Name
-[string]$AD1DCName = $DataSource.Configuration.AD1.DC.Name
+[string]$AD1DCName = $DataSource.Configuration.AD1.DC
+[string]$AD1DN = $DataSource.Configuration.AD1.DN
 [string]$AD1GroupFilter = $DataSource.Configuration.AD1.Filter.Pattern
+[array]$AD1OUList = $DataSource.Configuration.AD1.OUList
 
 # dump Variables used:
 Write-Log -Message "Dumping read values to Log..."
 Write-Log -Message ('Current User Context:            {0}' -f $CurrentUser)
 Write-Log -Message ('AD1 DC Name:                     {0}' -f $AD1DCName)
-Write-Log -Message ('AD1 Group Filter:                {0}' -f $AD1GroupFilter)
+Write-Log -Message ('AD1 DN:                          {0}' -f $AD1DN)
+#Write-Log -Message ('AD1 Group Filter:                {0}' -f $AD1GroupFilter)
+Write-Log -Message ('AD1 OU List:                     {0}' -f $AD1OUList)
 #foreach ($Service in $DataSource.Configuration.Service){Write-Log -Message ('Service Name:                    {0}' -f $Service.Name)}
 Write-Log -Message "end region read data from XML file"
 #endregion
@@ -159,13 +164,62 @@ if (!($AD1CredsExist)) {
 }
 Write-Log -Message ('Credentials of {0} successfully imported' -f $AD1Creds.UserName)
 
-##
-[array]$AD1Groups = @()
-$AD1Groups = Get-Group -Server $AD1DCName -Credentials $AD1Creds -Pattern $AD1GroupFilter
+## read groups from AD1
+[array]$AD1GroupsToProcess = @()
+#[array]$OUs = @()
+$collection = $AD1OUList.Split(',')
+
+##query groups for each OU found
+foreach ($currentItemName in $collection) {
+  <# $currentItemName is the current item #>
+  $currentItemName = $DataSource.Configuration.AD1.$currentItemName
+  $SearchPath = "OU=" + $currentItemName.Name + "," + $AD1DN
+  $Patternlist = ($currentItemName.GroupNames).Split(',')
+  
+  ##query only groups that match specified patterns
+  foreach ($Pattern in $Patternlist) {
+    <# $Pattern is tPatternlist$Patternlist item #>
+    $SearchPattern = $Pattern + $currentItemName.Name + "." + $currentItemName.Pattern + "*"
+    $GroupsReturned = Get-Group -Server $AD1DCName -Credentials $AD1Creds -Pattern $SearchPattern -SearchBase $SearchPath
+    $AD1GroupsToProcess += $GroupsReturned
+  }
+
+  ##cleanup
+  Remove-Variable -Name currentItemName
+  Remove-Variable -Name GroupsReturned
+  Remove-Variable -Name Pattern
+  Remove-Variable -Name Patternlist
+  Remove-Variable -Name Searchpath
+  Remove-Variable -Name SearchPattern
+}
+
+##cleanup query groups
+Remove-Variable -Name collection
+
+Write-Log -Message ('Querying groups done. Found {0} group(s) to process.' -f $AD1GroupsToProcess.count)
+
+##query group users
+Write-Log -Message "getting users from groups"
+[array]$AD1UsersToProcess = @()
+$collection = $AD1GroupsToProcess
+foreach ($currentItemName in $collection) {
+  <# $currentItemName is the current item #>
+  [array]$FoundUsers = @()
+  $FoundUsers = Get-ADGroupMember -Identity $currentItemName
+  $AD1UsersToProcess += $FoundUsers
+
+  Remove-Variable -Name currentItemName
+  Remove-Variable -Name FoundUsers
+}
+
+##cleanup query users
+Remove-Variable -Name collection
+
+Write-Log -Message ('Querying users done. Found {0} users(s) to process.' -f $AD1UsersToProcess.count)
 
 Write-Log -Message "cleanup"
-Remove-Variable -Name ADGroups
-Remove-Variable -Name AD1Groups
+#Remove-Variable -Name ADGroups
+#Remove-Variable -Name AD1Groups
 Remove-Variable -Name AD1CredsExist
 
 Write-Log -Message "end region Read Info from AD1"
@@ -173,6 +227,8 @@ Write-Log -Message "end region Read Info from AD1"
 
 
 #region Cleanup
+Remove-Variable -Name AD1GroupsToProcess
+Remove-Variable -Name AD1UsersToProcess
 Remove-Variable -Name DataSource
 
 #endregion
